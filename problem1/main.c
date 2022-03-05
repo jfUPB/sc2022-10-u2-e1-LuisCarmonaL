e86a4d15f177ddf6d590fe2ade58e7904ba03f42
LuisCarmona <luisfelipe.carmona.lopez@gmail.com>
2022-03-05 18:21:34 -0500

    A medias segundo punto V2

--- problem1/main.c
+++ problem1/main.c
@@ -55,13 +55,12 @@ for(int i = 0; i<numeroUsuario;i++){
 
 void arrayCommon(struct array *arrIn1, struct array *arrIn2, struct array *arrOut)
 {
-    int datos[20];
-    int datoPosicion = 0;
+    int datoPosicion = 0, i, j;
     
     arrOut->pdata = 0;
 
     for(i = 0; i < arrIn1->size; i++){
-        for(j = 0; j < arrIn2->size; j++){
+        for( j = 0; j < arrIn2->size; j++){
             if(*(arrIn1->pdata+i)== *(arrIn2->pdata+i)){
 
             }
